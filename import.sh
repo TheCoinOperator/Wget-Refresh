bd438b323b5f0a3b11348eafdcd34c1e267fca0f
TheCoinOperator <102594291+TheCoinOperator@users.noreply.github.com>
2022-03-29 13:08:53 +0100

    Update import.sh
    Added a folder check, just to not break anything, listed bugs

--- import.sh
+++ import.sh
@@ -2,7 +2,13 @@
 
 #this script will rename all existing files move them download the replacement and rename them
 
-#this is where variables go
+#Authored by Sophie Macfarlane 29/03/2022
+
+#known bugs: Nothing to check if the version is downloadable, will not work if you between renaming the old file and moving the old file the day changes
+
+#this changes the directory so the script is 100% being run in the same place
+
+cd /var/www/html/bc/softphones
 
 
 # this section takes the version name you will be downloading
@@ -13,9 +19,6 @@ read vername
 
 version=$vername
 
-# this is a test feature to make sure the version is being stored correctly, remove before publishing
-
-echo $version
 
 #this section will be dealing with the existing files, the first section will rename them oldver.fileextension+date
 
@@ -50,5 +53,4 @@ mv gloCOM-${version}.exe Communicator.exe
 mv gloCOM-${version}.dmg Communicator.dmg
 
 mv gloCOM.msi Communicator.msi
-
 
